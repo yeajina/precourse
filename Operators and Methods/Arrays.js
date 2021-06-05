f4ee35f03fa69b980248f168ed6ef9ac978478bb
Jina <68632320+yeajina@users.noreply.github.com>
2021-06-04 22:46:56 -0400

    Update Arrays.js

--- Operators and Methods/Arrays.js
+++ Operators and Methods/Arrays.js
@@ -61,3 +61,97 @@ function reassignAnElement(array, index, newValue) {
 
 var resultArray = reassignAnElement(['desk', 'lamp', 'toy'], 2, 'textbook');
 console.log('should replace "toy" with "textbook":', resultArray);
+
+
+// 4. Getting length of an array (.length)
+
+var lengthOfArray = [1, 2, 3, 4].length;
+console.log('lengthOfArray:', lengthOfArray);
+
+var legends = ["Alan Turing", "Ada Lovelace", "Al-Kwarizmi", "Edsger Dijkstra"]
+var lengthOfLegendsArray = legends.length;
+console.log('lengthOfLegendaryArray:', lengthOfLegendsArray);
+
+function getArrayLength(array) {
+    var length = array.length;
+    return length;
+}
+
+var resultLength1 = getArrayLength([1, 3, 4, 5, 7]);
+console.log('should log 5:', resultLength1);
+
+var resultLength2 = getArrayLength(['a', 'b', 'c', 'd']);
+console.log('should log 4:', resultLength2);
+
+
+// 5. Getting last element of an array (.length -1)
+
+var lastElement = [1, 2, 3, 4, 5] [[1, 2, 3, 4, 5].length - 1];
+console.log('lastElement:', lastElement);
+
+var numberSequence = [1, 2, 3, 4, 5];
+var lastIndex = numberSequence.length - 1;
+
+var lastElement = numberSequence[lastIndex] //need [] around .length - 1
+console.log('lastElement:', lastElement);
+
+function getLastElement(array) {
+    var lastIndex = array.length - 1;
+    var lastElement = array[lastIndex];
+    return lastElement;
+}
+
+var resultLastElement1 = getLastElement([1, 2, "buckle", "my", "shoe"]);
+console.log('should log "shoe":', resultLastElement1);
+
+var resultLastElement2 = getLastElement([3, 4, "shut", "the", "door"]);
+console.log('should log "door":', resultLastElement2);
+
+
+// 6. Adding element to back of an array (.push)
+
+var array = [1, 2, 3, 4];
+array.push(5);
+console.log('array with added element:', array);
+
+var messages = ['Hey', 'Sounds good', 'See you then'];
+var newMessage = "Omw";
+messages.push(newMessage);
+console.log('list of messages with new message:', messages);
+
+function applyPush (array, element) {
+    array.push(element);
+    return array;
+}
+
+var resultArray1 = applyPush([1, 17, 29], 34);
+console.log('should log [1, 17, 29, 34]:', resultArray1);
+
+var resultArray2 = applyPush(['abc', 'def'], 'ghi');
+console.log('should log ["abc", "def", "ghi"]:', resultArray2);
+
+
+// 7. Removing element from back of an array (.pop)
+
+var array= [1, 2, 3, 4];
+var elementPopped = array.pop();
+console.log('array without removed element:', array);
+console.log('element removed:', elementPopped);
+
+var weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Sunday'];
+var dayPopped = weekdays.pop();
+console.log('week days with last element removed:', weekdays);
+console.log('last day removed:', dayPopped);
+
+function applyPop(array) {
+    var popped = array.pop(array)); //array.pop is the function that (array) is being applied with
+    return popped;
+}
+
+var resultElement1 = applyPop([1, 171, 129]);
+console.log('should log 129:', resultElement1);
+
+var resultElement2 = applyPop(['islands', 'peninsulas', 'pacific']);
+console.log('should log pacific:', resultElement2);
+
+
